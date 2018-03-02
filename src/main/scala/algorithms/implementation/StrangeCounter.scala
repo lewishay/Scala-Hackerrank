bc307b387fb3101268c71ba2f317ec1b6764c700
lewis-hay <lewis.hay@digital.hmrc.gov.uk>
2018-03-02 16:48:51 +0000

    Added the StrangeCounter object

---
+++ src/main/scala/algorithms/implementation/StrangeCounter.scala
@@ -0,0 +1,26 @@
+package algorithms.implementation
+
+// See https://www.hackerrank.com/challenges/strange-code/problem for a description of this problem.
+
+object StrangeCounter {
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    println("Please enter the number:")
+    val t = sc.nextLong()
+    println(strangeCode(t))
+  }
+
+  def strangeCode(t: Long): Long = {
+    if(t > 3) {
+      (t - counterGroupMax(t) - 1L) * -1L
+    } else {
+      Seq(3L, 2L, 1L).indexOf(t) + 1L
+    }
+  }
+
+  @annotation.tailrec
+  def counterGroupMax(limit: Long, inc: Long = 6L, currentMax: Long = 3L): Long = currentMax match {
+    case x if x >= limit => x
+    case x => counterGroupMax(limit, inc * 2L, x + inc)
+  }
+}
